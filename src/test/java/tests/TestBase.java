87a584a22d3a7b248f8442983dff3715e5b9563c
Ilya_Malykh <ferras2007@yandex.ru>
2021-03-14 22:35:15 +0300

    fix testbase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,16 @@
 package tests;
 
+import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static config.ConfigHelper.isVideoOn;
 import static helpers.AttachmentsHelper.*;
 import static helpers.DriverHelper.*;
 
+@ExtendWith(AllureJunit5.class)
 public class TestBase {
 
     @BeforeAll
